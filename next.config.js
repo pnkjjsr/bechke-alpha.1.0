4c6959fd660cc63e33a0ac75e61c1514ab65c669
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2022-09-06 12:06:46 +0530

    nextjs config rewrite function to get custome domain setup.

--- next.config.js
+++ next.config.js
@@ -8,7 +8,26 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
     prependData: `@import "base.scss";`
-  }
+  },
+
+  async rewrites() {
+    return [
+      // {
+      //   source: '/about',
+      //   destination: '/',
+      // },
+      {
+        source: 'bechke-alpha-1-0-pnkjjsr.vercel.app',
+        has: [
+          {
+            type: 'host',
+            value: 'vercel.app',
+          },
+        ],
+        destination: '/about',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
